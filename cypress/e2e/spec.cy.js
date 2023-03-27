0c081a9f6c8ae97f9b40aea8ec2b0eba5de93f20
Derek Yeh <derek.yeh91@gmail.com>
2023-03-27 14:03:43 -0700

    Added test to cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('passes another test', () => {
+    cy.visit('https://www.google.com')
+  })
 })
