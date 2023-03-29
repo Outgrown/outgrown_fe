8eda8943cd33d3ff05671adac3e74d91d787d8e8
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-03-28 23:12:37 -0400

    Add a card component barebones for a single card item

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import logo from '../../Images/logo.svg'
 import './App.css';
 import Form from '../Form/Form';
+import Card from '../Card/Card';
 
 function App() {
   return (
     <div className="App">
       <Form/>
+      <Card/>
     </div>
   );
 }
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './Card.css'
+
+const Card = () => {
+return( <section className="clothing-card">
+<section>
+  <h2> image area </h2>
+</section>
+<section>
+  <h2> clothing Name: </h2>
+  <h2> Price: </h2>
+  <h3> Owner:</h3>
+  <p>-------------------------------</p>
+  <div> 
+    <h3>Age Group </h3>
+    <h3> change me! </h3>
+  </div>
+  <div>
+    <h3> Gender </h3>
+    <h3>Change me </h3>
+  </div>
+  <div>
+    <h3>Type</h3>
+    <h3> change me </h3>
+  </div>
+  <div>
+    <h3> Condition</h3>
+    <h3> Change me </h3>
+  </div>
+  <div>
+    <h3> Description</h3>
+    <p> Special sentence text... </p>
+  </div>
+  <div>
+    <h3> Condition</h3>
+    <h3> Change me </h3>
+  </div>
+  <button> Add to Profile </button>
+  </section>
+</section>
+
+)
+}
+export default Card; 
+
