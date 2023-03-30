41126b7ac29c78e584c22c104b6956b1fe2001ea
Max Lange <max@langes.us>
2023-03-30 13:50:03 -0400

    Import query + log successful call using useQuery

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_ARTICLES } from '../../apiCalls';
 
 function App() {
+  const { loading, error, data } = useQuery(GET_ALL_ARTICLES);
+
+  console.log(data)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
