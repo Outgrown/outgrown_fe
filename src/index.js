8ca01fd595afdeab33ba3dc9b316396be3cdca03
Max Lange <max@langes.us>
2023-03-30 13:50:40 -0400

    Remove strictmode + Apollo Client setup

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from 'react-router-dom';
+
+const client = new ApolloClient({
+  uri: 'https://2453ced7-a985-4472-8b7d-20ed31e41526.mock.pstmn.io',
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
-  <React.StrictMode>
+<ApolloProvider client={client}>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
-</BrowserRouter>
+  </BrowserRouter>
+</ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
