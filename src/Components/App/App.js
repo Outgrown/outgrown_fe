8d8cc83099edbe60092cbec2e2d0ec16f3a36925
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-30 13:12:33 -0600

    Import and conditionally render Loading component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useQuery, gql } from '@apollo/client';
+import React, { useEffect } from 'react';
+import { GET_ALL_ARTICLES, GET_USER } from '../../apiCalls';
+import Loading from '../Loading/Loading';
 import './App.css';
 
 function App() {
+
+  const {loading, error, data} = useQuery(GET_ALL_ARTICLES)
+
+  console.log(data)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <Loading />}
     </div>
   );
 }
