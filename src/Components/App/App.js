ce9a7bd6b662724a67577501e461da26c148f2d2
Max Lange <max@langes.us>
2023-03-30 17:13:09 -0400

    Move all articles query to Marketplace component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,17 +4,10 @@ import Nav from '../Nav/Nav'
 import Form from '../Form/Form';
 import Details from '../Details/Details';
 import Loading from '../Loading/Loading';
-
 import { Switch, Route } from 'react-router-dom';
-import { useQuery, gql } from '@apollo/client';
-import { GET_ALL_ARTICLES } from '../../apiCalls';
 import './App.css';
 
 const App = () => {
-  const {loading, error, data} = useQuery(GET_ALL_ARTICLES)
-
-  console.log(data)
-
   return (
     <div className="App">
       <Nav />
