ef1e8cdfc026d62e32e137d4bcc2f6165ca06bf7
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-30 16:29:41 -0600

    Add basic structure for card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Card.css'
 
-const Card = () => {
-  return( <>
-  </>
+const Card = ({id, name, imageLink, altImage, price}) => {
+  return( 
+  <Link to={`/details/${id}`}>
+    <p>{name}</p>
+    <img src={imageLink} alt={altImage}/>
+    <p>{price}</p>
+  </Link>
   )
 }
 export default Card; 
