005138d0c32259a20258b2817f008d50400e9437
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-30 13:10:24 -0600

    Fix graphql

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./Components/App/App";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://2453ced7-a985-4472-8b7d-20ed31e41526.mock.pstmn.io',
-  cache: new InMemoryCache()
-})
+  uri: "https://2453ced7-a985-4472-8b7d-20ed31e41526.mock.pstmn.io",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
