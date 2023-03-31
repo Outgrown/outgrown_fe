20b8a5495f89a5bb247111ebbefa37b6231a5b4b
Max Lange <max@langes.us>
2023-03-31 17:47:59 -0400

    Set new filter hooks in Marketplace component, and pass to Form component

--- src/Components/Marketplace/Marketplace.js
+++ src/Components/Marketplace/Marketplace.js
@@ -9,6 +9,10 @@ import { GET_ALL_ARTICLES } from '../../apiCalls';
 
 export default function Marketplace() {
   const [allArticles, setAllArticles] = useState([])
+  const [articleType, setArticleType] = useState('')
+  const [ageGroup, setAgeGroup] = useState('')
+  const [gender, setGender] = useState('')
+
   const {loading, error, data} = useQuery(GET_ALL_ARTICLES)
   
   useEffect(() => {
@@ -19,6 +23,7 @@ export default function Marketplace() {
   if(error) {
     info = <Redirect to="/error" />
   } else if (!loading && allArticles) {
+    // console.log(new Set(allArticles.map(art => art.articleType)))
     info = allArticles.map(art => 
       <Card 
         key={art.id}
@@ -34,7 +39,11 @@ export default function Marketplace() {
 
   return (
     <>
-      <Form />
+      <Form 
+        setArticleType={setArticleType} 
+        setAgeGroup={setAgeGroup} 
+        setGender={setGender}
+      />
       {loading ? <Loading /> :
       <div className="market-container">
         {info}
