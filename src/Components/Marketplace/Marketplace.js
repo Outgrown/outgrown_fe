0f48c7cdd9565df28b15e8e14de2015feccd4de3
Max Lange <max@langes.us>
2023-03-31 15:35:02 -0400

    Update Marketplace component to use newly created Card component

--- src/Components/Marketplace/Marketplace.js
+++ src/Components/Marketplace/Marketplace.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Form from "../Form/Form";
 import Loading from "../Loading/Loading";
 import Card from "../Card/Card"
+import "./Marketplace.css"
 import { Redirect } from "react-router-dom";
 import { useQuery } from '@apollo/client';
 import { GET_ALL_ARTICLES } from '../../apiCalls';
@@ -19,18 +20,17 @@ export default function Marketplace() {
     info = <Redirect to="/error" />
   } else if (!loading && allArticles) {
     info = allArticles.map(art => 
-      <p>{art.name}</p>
+      <Card 
+        key={art.id}
+        id={art.id}
+        name={art.name}
+        ageGroup={art.ageGroup}
+        imageLink={art.imageLink}
+        altImage={art.altImage}
+        price={art.price}
+       />
     )
   }
-  //      <Card 
-  //       key={art.id}
-  //       id={art.id}
-  //       name={art.name}
-  //       ageGroup={art.ageGroup}
-  //       imageLink={art.imageLink}
-  //       altImage={art.altImage}
-  //       price={art.price}
-  //      />
 
   return (
     <>
