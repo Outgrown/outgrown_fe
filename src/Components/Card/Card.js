ac59c83209108b6798c48e2120c196617a14b06d
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-31 10:44:38 -0600

    Refactor card layout to use flexto eliminate white spaces

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './Card.css'
 
-const Card = ({id, name, imageLink, altImage, price}) => {
+const Card = ({id, name, imageLink, altImage, ageGroup, price}) => {
   return( 
-  <Link to={`/details/${id}`}>
-    <p>{name}</p>
-    <img src={imageLink} alt={altImage}/>
-    <p>{price}</p>
+  <Link to={`/details/${id}`} className='link'>
+    <div className="card">
+      <img src={imageLink} alt={altImage}/>
+      <div className="text">
+        <p className="name">{name}</p>
+        <p className="price">{price}</p>
+        <p className="age">Size: {ageGroup}</p>
+      </div>
+    </div>
   </Link>
   )
 }
