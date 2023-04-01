859112c330f05c415462708ad4894c1f089256b8
Derek Yeh <derek.yeh91@gmail.com>
2023-04-01 11:01:41 -0700

    Add navlink

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Nav.css'
+import { NavLink } from 'react-router-dom'
 
 
 const Nav = () => {
@@ -9,17 +10,25 @@ const Nav = () => {
             <h1>Outgrown</h1>
             <div className='nav-bar'>
                 <div className='nav-grid'>
-                    <p>MARKETPLACE</p>
-                    <p>BABY</p>
-                    <p>GIRL</p>
-                    <p>BOY</p>
+                    <NavLink to='/marketplace' className='marketplace-button'>
+                        <p>MARKETPLACE</p>
+                    </NavLink>
+                    <NavLink to='/marketplace/baby' className='baby-nav-button'>
+                        <p>BABY</p>
+                    </NavLink>
+                    <NavLink to='/marketplace/girl' className='girl-nav-button'>
+                        <p>GIRL</p>
+                    </NavLink>
+                    <NavLink to='/marketplace/boy' className='boy-nav-button'>
+                        <p>BOY</p>
+                    </NavLink>
                 </div>
                 <div className='dropdown'>
                     <button className='dropbutton'>Users</button>
                     <div className='dropdown-options'>
-                        <a href=''>user1</a>
-                        <a href=''>user2</a>
-                        <a href=''>user3</a>
+                        <a href='/profile/user1'>user1</a>
+                        <a href='/profile/user2'>user2</a>
+                        <a href='/profile/user3'>user3</a>
                     </div>
                 </div>
             </div>
