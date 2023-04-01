ca8d85d3c03eed90be9d7283ffe214da2e6589af
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-04-01 19:30:20 -0400

    git status

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -6,7 +6,7 @@ import Loading from "../Loading/Loading";
 
 
 const Details = () => {
-const [desiredItem, setDesiredItem] = useState(null)
+  const [desiredItem, setDesiredItem] = useState(null)
 
   const {loading, error, data} = useQuery(GET_ARTICLE,{
       variables:{
@@ -16,10 +16,10 @@ const [desiredItem, setDesiredItem] = useState(null)
   })
 
 
- useEffect(() => {
-  
-  setDesiredItem(data)
-},[loading,error])
+  useEffect(() => {
+    
+    setDesiredItem(data)
+  },[loading,error])
 
 
 
@@ -33,29 +33,32 @@ const [desiredItem, setDesiredItem] = useState(null)
       </article>
       <section className="details-cont">
         <div className="vital-info"> 
-          <h2> Cost: ${data.findArticle.price}</h2>
+          <h2> {data.findArticle.name}</h2>
           <h3> Owner: {data.findArticle.user.name} </h3>
           <h3> Status:{data.findArticle.status} </h3>
+          <h4> Cost: ${data.findArticle.price}</h4>
         </div>
-        <div className="trait-wrap"> 
-          <h3>Age Group </h3>
-          <h3> {data.findArticle.ageGroup}</h3>
-        </div>
-        <div className="trait-wrap">
-          <h3> Gender </h3>
-          <h3> {data.findArticle.gender}</h3>
-        </div>
-        <div className="trait-wrap">
-          <h3>Type</h3>
-          <h3>{data.findArticle.articleType} </h3>
-        </div>
-        <div className="trait-wrap">
-          <h3> Condition</h3>
-          <h3> {data.findArticle.condition} </h3>
-        </div>
-        <div className="trait-wrap">
-          <h3> Color </h3>
-          <h3> {data.findArticle.color} </h3>
+        <div className="info-wrap"> 
+          <div className="trait-wrap"> 
+            <h3>Age Group </h3>
+            <h3> {data.findArticle.ageGroup}</h3>
+          </div>
+          <div className="trait-wrap">
+            <h3> Gender </h3>
+            <h3> {data.findArticle.gender}</h3>
+          </div>
+          <div className="trait-wrap">
+            <h3>Type</h3>
+            <h3>{data.findArticle.articleType} </h3>
+          </div>
+          <div className="trait-wrap">
+            <h3> Condition</h3>
+            <h3> {data.findArticle.condition} </h3>
+          </div>
+          <div className="trait-wrap">
+            <h3> Color </h3>
+            <h3> {data.findArticle.color} </h3>
+          </div>
         </div>
         <div className='desc-area'>
           <h3> Description</h3>
@@ -65,10 +68,7 @@ const [desiredItem, setDesiredItem] = useState(null)
           <button className="details-btn"> Add To Profile </button>
         </div>
         </section>
-      </section> }
-    </section>
-    
-    
-    )
+    </section> }
+  </section> )
 }
 export default Details; 
