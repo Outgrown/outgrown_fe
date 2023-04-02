f5935441c756482ab332e7b4cc7348c2de6080f3
Max Lange <max@langes.us>
2023-04-01 21:56:55 -0400

    Update Nav component + styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,57 +1,47 @@
 import React from 'react'
 import './Nav.css'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import logo from '../../assets/OutGrownLogo.png'
 import Form from '../Form/Form'
 import Marketplace from '../Marketplace/Marketplace'
 
 const Nav = () => {
+  // const elements = [
+  //     <p key="1" value="marketplace">MARKETPLACE</p>,
+  //     <p key="2" value="baby">Baby (0 - 2)</p>,
+  //     <p key="3" value="toddler">Toddler (2 - 4)</p>,
+  //     <p key="4" value="little_kid">Little Kid (5 - 7)</p>,
+  //     <p key ="5" value="big_kid">Big Kid (8+)</p>
+  // ]
 
-    // const elements = [
-    //     <p key="1" value="marketplace">MARKETPLACE</p>,
-    //     <p key="2" value="baby">Baby (0 - 2)</p>,
-    //     <p key="3" value="toddler">Toddler (2 - 4)</p>,
-    //     <p key="4" value="little_kid">Little Kid (5 - 7)</p>,
-    //     <p key ="5" value="big_kid">Big Kid (8+)</p>
-    // ]
+  // const wrapElements = elements.map((element, index) => {
+  //     <NavLink key={index} to={`/marketplace/${element}`} className='marketplace-button'>
+  //         {element}
+  //     </NavLink>
+  // })
 
-    // const wrapElements = elements.map((element, index) => {
-    //     <NavLink key={index} to={`/marketplace/${element}`} className='marketplace-button'>
-    //         {element}
-    //     </NavLink>
-    // })
-
-    return (
-        <section className='nav-container'>
-            <h1>Outgrown</h1>
-            <div className='nav-bar'>
-                <div className='nav-grid'>
-                    <NavLink to='/marketplace' className='marketplace-button'>
-                        <p value="marketplace">MARKETPLACE</p>
-                    </NavLink>
-                    <NavLink to='/marketplace/baby' className='baby-nav-button'>
-                        <p value="baby">Baby (0 - 2)</p>
-                    </NavLink>
-                    <NavLink to='/marketplace/todler' className='toddler-nav-button'>
-                        <p value="toddler">Toddler (2 - 4)</p>
-                    </NavLink>
-                    <NavLink to='/marketplace/little_kid' className='Little-kid-nav-button'>
-                        <p value="little_kid">Little Kid (5 - 7)</p>
-                    </NavLink>
-                    <NavLink to='/marketplace/big_kid' className='big-kid-nav-button'>
-                        <p value="big_kid">Big Kid (8+)</p>
-                    </NavLink>
-                </div>
-                <div className='dropdown'>
-                    <button className='dropbutton'>Users</button>
-                    <div className='dropdown-options'>
-                        <a href='/profile/user1'>user1</a>
-                        <a href='/profile/user2'>user2</a>
-                        <a href='/profile/user3'>user3</a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className='nav-container'>
+      <Link to="/" className="logo-link"><img className="logo" src={logo}/></Link>
+      <div className='nav-bar'>
+        <div className='nav-grid'>
+          <NavLink exact to='/marketplace' className='nav-button' activeClassName="selected">MARKETPLACE</NavLink>
+          <NavLink to='/marketplace/baby' className='nav-button' activeClassName="selected">BABY</NavLink>
+          <NavLink to='/marketplace/todler' className='nav-button' activeClassName="selected">TODDLER</NavLink>
+          <NavLink to='/marketplace/little_kid' className='nav-button' activeClassName="selected">LITTLE KID</NavLink>
+          <NavLink to='/marketplace/big_kid' className='nav-button' activeClassName="selected">BIG KID</NavLink>
+        </div>
+        <div className='dropdown'>
+          <button className='dropbutton'>CHANGE USER</button>
+          <div className='dropdown-options'>
+            <a href='/profile/user1'>User 1</a>
+            <a href='/profile/user2'>User 2</a>
+            <a href='/profile/user3'>User 3</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Nav;
