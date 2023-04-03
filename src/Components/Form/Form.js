f80d7fc893c366efcc988d24242b3abd72d9373c
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-04-03 14:10:13 -0400

    add classnames to form selects to get fontstyles

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -34,44 +34,35 @@ const Form = ({navParam, setArticleType, setAgeGroup, setGender}) => {
       <div className="clothing-article-sel"> 
         <label> Article Types </label>
           <select onChange={(e) => handleSelectedOptions(setArticle,e)}> 
-            <option value={''}> All </option>
-            <option value={'tops'}> Tops </option>
-            <option value={'bottoms'}> Bottoms </option>
-            <option value={'formal'}> Formal</option>
-            <option value={'onesies'}>Onesie</option>
-            <option value={'activewear'}> Active Wear</option>
-            <option value={'other'}> Other </option>
+            <option className="form-opts" value={''}> All </option>
+            <option className="form-opts" value={'tops'}> Tops </option>
+            <option className="form-opts" value={'bottoms'}> Bottoms </option>
+            <option className="form-opts" value={'formal'}> Formal</option>
+            <option className="form-opts" value={'onesies'}>Onesie</option>
+            <option className="form-opts" value={'activewear'}> Active Wear</option>
+            <option className="form-opts" value={'other'}> Other </option>
           </select>
         </div>
       <div className="age-group-sel">
         <label> Age Groups </label>
         <select className="form-select" value={kidAge} onChange={(e) => handleSelectedOptions(setKidAge,e)}> 
-          <option value={''}> All </option>
-          <option value={'baby'}> Baby(0-2yr) </option>
-          <option value={'todler'}> Toddler(2-4yr)</option>
-          <option value={'little_kid'}> Little Kid (5-7yr)</option>
-          <option value={'big_kid'}> Big Kid (8+)</option>
+          <option className="form-opts" value={''}> All </option>
+          <option className="form-opts" value={'baby'}> Baby(0-2yr) </option>
+          <option className="form-opts" value={'todler'}> Toddler(2-4yr)</option>
+          <option className="form-opts" value={'little_kid'}> Little Kid (5-7yr)</option>
+          <option className="form-opts" value={'big_kid'}> Big Kid (8+)</option>
         </select>
         </div>
         <div className="gender-sel">
         <label> Gender </label>
           <select onChange={(e) => handleSelectedOptions(setKidGender,e)}> 
-            <option value={''}> All </option>
-            <option value={'male'}> Boy </option>
-            <option value={'female'}> Girl </option>
-            <option value={'unisex'}> Unisex</option>    
+            <option className="form-opts"value={''}> All </option>
+            <option className="form-opts" value={'male'}> Boy </option>
+            <option className="form-opts"value={'female'}> Girl </option>
+            <option className="form-opts"value={'unisex'}> Unisex</option>    
           </select>
         </div>
     </form>
-      {/* <div className="search-area">
-        <input
-          placeholder="Search by Title"
-          name='searchedItem'
-          onChange={(e) => handleSelectedOptions(setSearchedItem,e)}
-        /> 
-        <button> Search </button>
-      </div> */}
-
   </section>
 
 
