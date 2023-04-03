3f6b9aa7febabf958ee77bd7ce2bbc923ddcb2ab
Ian Nordby <ian.c.nordby@gmail.com>
2023-04-03 11:24:48 -0600

    Add message to Error

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './Error.css'
 
-const Error = (message) => {
+const Error = ({message}) => {
   return (
     <div className="error-container">
-      <img src="https://www.matchclothing.co.uk/user/baby-big-clothes-6.jpg"/>
+      <p>We want you looking fly, but...</p>
+      <img className="error-image" src="https://www.matchclothing.co.uk/user/baby-big-clothes-6.jpg"/>
       <p>{message}</p>
     </div>
   )
