f403ad63b9168d78068450e44753fec9e22d85d2
Max Lange <max@langes.us>
2023-04-04 17:02:03 -0400

    Stubbing foundation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Marketplace spec', () => {
+  beforeEach(() => {
+    cy.intercept('POST', 'https://outgrown-be.herokuapp.com/graphql', (req) => {
+      if(req.body.operationName.includes('findArticles')) {
+        req.reply({ fixture: 'findArticles.json' })
+      }
+    }).as("testing");
+    cy.visit('http://localhost:3000/marketplace')
   })
 
   it('passes another test', () => {
+    cy.wait('@testing')
     cy.expect(true).to.eq(true)
   })
 })
