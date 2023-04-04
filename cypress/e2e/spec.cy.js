d9e884782d3c04dc1917e117d0c627328b97d6c5
Max Lange <max@langes.us>
2023-04-04 17:48:03 -0400

    Update req.reply with imported helper function

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,10 @@
+import { constructJSON } from "../fixture_helper";
+
 describe('Marketplace spec', () => {
   beforeEach(() => {
     cy.intercept('POST', 'https://outgrown-be.herokuapp.com/graphql', (req) => {
       if(req.body.operationName.includes('findArticles')) {
-        req.reply({ fixture: 'findArticles.json' })
+        req.reply(constructJSON(req.body.variables.articleType, req.body.variables.ageGroup, req.body.variables.gender))
       }
     }).as("testing");
     cy.visit('http://localhost:3000/marketplace')
