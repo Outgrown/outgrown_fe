d50741556c889b0cc391384206a74290fda6b646
Ian Nordby <ian.c.nordby@gmail.com>
2023-04-04 19:13:30 -0600

    Add Modal component

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Modal = () => {
+  const [ModalIsOpen, setModalIsOpen] = useState(false)
+
+  return (
+    <div className="modal-container">
+      <div className="modal">
+
+      </div>
+    </div>
+  )
+}
+
+export default Modal
